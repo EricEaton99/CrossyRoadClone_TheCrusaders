e7c038f94658f563c07ece9b57e2fb9bc4db668c
fellbeast9 <Wring8@gmail.com>
2020-10-07 14:00:36 -0400

    UI audio

---
+++ Crossy Road - Project 1/Assets/Scripts/S_UIAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_UIAudio : MonoBehaviour
+{
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+    public AudioSource myFx;
+
+
+    public void ButtonHover()
+    {
+        myFx.PlayOneShot(hoverSound);
+    }
+
+    public void ButtonClick()
+    {
+        myFx.PlayOneShot(clickSound);
+    }
+}
